46dafc66443f12848f6b96bd1d754505aa211595
Kuro-P <837408039@qq.com>
2019-11-14 11:56:56 +0800

    rm /public

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Kuro-P.github.io
 My blog record. [https://kuro-p.github.io/](https://kuro-p.github.io/)
+
+# Branch develop
+if there's some files you don't want to commit, please use `.gitignore` not hexo-deployer-git.
---
+++ deploy/build.sh
@@ -0,0 +1,18 @@
+# hexo build (master)
+# hexo g
+# hexo deploy
+
+# git commit (develop)
+git status
+read -t 30 -p "Confirm summit changes(Y/N)?" confirm
+read -t 30 -p "Please enter commit message:" message
+if [ $confirm == "Y" ]
+then 
+    echo "Start commiting files to github"
+    git add .
+    git commit -m $message
+    git push
+else
+    echo "Develop files reserve abort."    
+fi 
+echo "Done."   
