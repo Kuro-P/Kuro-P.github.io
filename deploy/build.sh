10c8da491351ba7e0751dfd41dc1c5a2605795c7
Kuro-P <837408039@qq.com>
2019-12-27 15:36:45 +0800

    update build.sh(fix commit msg bug)

--- deploy/build.sh
+++ deploy/build.sh
@@ -1,18 +1,29 @@
-# hexo build (master)
-hexo g
-sudo hexo deploy
-
+#!/bin/bash
 # git commit (develop)
 git status
+git diff
 read -t 30 -p "Confirm summit changes(Y/N)?" confirm
-read -t 30 -p "Please enter commit message:" message
-if [ $confirm == "Y" ]
-then 
+
+if [ $(echo $confirm | tr '[A-Z]' '[a-z]')=="y" ]
+then
+    read -t 30 -p "Please enter commit message:" message
     echo "Start commiting files to github"
     git add .
     git commit -m "$message"
     git push
+
+    CONFIG_URL="_config.yml"
+    if [ -f ${CONFIG_URL} ]
+        then
+            echo "Start "
+            sed -i "" "s/commit_message/$message/g" ${CONFIG_URL}
+            # hexo build (master)
+            hexo g
+            sudo hexo deploy
+            sed -i "" "s/$message/commit_message/g" ${CONFIG_URL}
+    fi        
+
 else
     echo "Develop files reserve abort."    
 fi 
-echo "Done."   
+echo "Done."
